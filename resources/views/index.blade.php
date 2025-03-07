eb0a1385115c3103ea07ee8af4146b7850336f64
gkerenel <kerenel@outlook.com>
2025-03-07 10:48:46 +0200

    fixes,updates

---
+++ resources/views/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Twitter Clone Api</title>
+
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+
+            html, body {
+                width: 100%;
+                height: 100%;
+            }
+
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            h1 {
+                font-weight: bold;
+                font-family: sans-serif;
+                color: indianred;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>THIS IS A API SERVICES. PLEASE USE THE API ROUTES</h1>
+    </body>
+</html>
